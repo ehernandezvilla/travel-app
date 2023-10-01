e1fa8e3fa46125d047cd4b18ccb5d1616d001f90
Eduardo <ehernandezvilla@gmail.com>
2023-10-01 20:49:10 -0300

    feature: add local storage to save trip data

--- src/client/assets/js/localStorage.js
+++ src/client/assets/js/localStorage.js
@@ -3,15 +3,35 @@ const remove_btn = document.getElementById('remove_trip');
 const ul = document.querySelector('ul');
 const input = document.getElementById('city');
 
+//LocalStorage
+
+let = itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+localStorage.setItem('items', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('items'));
+
+
 const liMaker = (text) => {
     const li = document.createElement('li');
     li.textContent = text;
     ul.appendChild(li);
-} 
+}
 
 save_btn.addEventListener('click', function() { 
-    liMaker(input.value)
-    // input.value = '';
+    console.log('trip saved!')
+    itemsArray.push(input.value);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    liMaker(input.value);
+});
+
+data.forEach((item) => {
+    liMaker(item);
 });
 
 
+remove_btn.addEventListener('click', function() {
+    localStorage.clear();
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+});
